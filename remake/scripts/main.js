3a889b0032af1dff07c5dfbc260964ebc29e135a
shasharala <shasharala@outlook.com>
2014-05-23 02:59:00 -0700

    added some more script to handle appearing/disappearing text

--- remake/scripts/main.js
+++ remake/scripts/main.js
@@ -1,5 +1,5 @@
 //------------COOKIES------------
-//all cookies from shasharala.tk begin with "shasha_" automagically
+//all cookies from game begin with "sha_deeper_" automagically
 function checkCookie(name, value) {
 	if(getCookie(name) == value) {
 		return true;
@@ -9,47 +9,102 @@ function checkCookie(name, value) {
 	}
 }
 
-//all cookies from shasharala.tk begin with "shasha_" automagically
+//all cookies from game begin with "sha_deeper_" automagically
 function getCookie(name) {
 	var cookieStr = document.cookie;
 	var returnValue = "";
-	var searchFor = "shasha_" + name + "=";
-	var startIndex = cookieStr.indexOf(searchFor)
+	var searchFor = "sha_deeper_" + name + "=";
+	var startIndex = cookieStr.indexOf(searchFor);
 	if(startIndex != -1) {
 		startIndex += searchFor.length;
-		endIndex = cookieStr.indexOf(";", startIndex)
+		endIndex = cookieStr.indexOf(";", startIndex);
 		if (endIndex == -1) {
 			endIndex = cookieStr.length;
-  			returnValue = cookieStr.substring(startIndex, endIndex)
+  			returnValue = cookieStr.substring(startIndex, endIndex);
   		}
   	}
   	return returnValue;
 }
 
-//all cookies from shasharala.tk begin with "shasha_" automagically
+//all cookies from game begin with "sha_deeper_" automagically
 function setCookie(name, value, expires) {
 	var d = new Date();
 	d.setTime(d.getTime() + (expires*24*60*60*1000));
 	var e = "expires " + d.toUTCString();
-	document.cookie = "shasha_" + name + "=" + value + "; " + e;
+	document.cookie = "sha_deeper_" + name + "=" + value + "; " + e;
 }
 
 //------------UNIVERSAL------------
-//anything that needs to be hidden outside of the user clicking a button will use hide()
-function hide(name) {
-	document.getElementById(name).style.display = "none";
+
+function replace(name1, name2) {
+	hide(name1);
+	show(name2);
 }
-//buttons will use hideParent(this). see hide() for anything that needs to be hidden outside of the user clicking a button
-function hideParent(name) {
-	name.parentNode.style.display = "none";
+
+function replaceF(name1, name2) {
+	fadeOut(name1);
+	fadeIn(name2);
+}
+
+function fadeIn(name) {
+	var nameStr = name + "";
+	var searchFor = "object";
+	var t = nameStr.indexOf(searchFor);
+	if (t != -1) {
+		show(name);
+		name.parentNode.style.animation = "invisToVis 1s 1 forwards";
+		//Safari & Chrome
+		name.parentNode.style.WebkitAnimation = "invisToVis 1s 1 forwards";
+	}
+	else {
+		show(name);
+		document.getElementById(name).style.animation = "invisToVis 1s 1 forwards";
+		//Safari & Chrome
+		document.getElementById(name).style.WebkitAnimation = "invisToVis 1s 1 forwards";
+	}
+}
+
+function fadeOut(name) {
+	var nameStr = name + "";
+	var searchFor = "object";
+	var t = nameStr.indexOf(searchFor);
+	if (t != -1) {
+		name.parentNode.style.animation = "visToInvis 1s 1 forwards";
+		//Safari & Chrome
+		name.parentNode.style.WebkitAnimation = "visToInvis 1s 1 forwards";
+		hide(name);
+	}
+	else {
+		document.getElementById(name).style.animation = "visToInvis 1s 1 forwards";
+		//Safari & Chrome
+		document.getElementById(name).style.WebkitAnimation = "visToInvis 1s 1 forwards";
+		hide(name);
+	}
+}
+
+function hide(name) {
+	var nameStr = name + "";
+	var searchFor = "object";
+	var t = nameStr.indexOf(searchFor);
+	if (t != -1) {
+		name.parentNode.style.display = "none";
+	}
+	else {
+		document.getElementById(name).style.display = "none";
+	}
 }
 
 function show(name) {
-	document.getElementById(name).style.display = "initial";
-}
-
-function showParent(name) {
-	name.parentNode.style.display = "initial";
+	var nameStr = name + "";
+	var searchFor = "object";
+	var t = nameStr.indexOf(searchFor);
+	if (nameStr.indexOf(searchFor) != -1) {
+		name.parentNode.style.display = "initial";
+	}
+	else {
+		document.getElementById(name).style.display = "initial";
+	}
+	
 }
 
 function clickBtn(name) {
