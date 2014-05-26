72c7443fc0ffa5f7d9e612c7336fef125b48ebbe
shasharala <shasharala@outlook.com>
2014-05-25 22:46:33 -0700

    removed cookies. will replace later with web storage if necessary. chromium browsers do not support local cookies

--- remake/scripts/main.js
+++ remake/scripts/main.js
@@ -1,39 +1,3 @@
-//------------COOKIES------------
-//all cookies from game begin with "sha_deeper_" automagically
-function checkCookie(name, value) {
-	if(getCookie(name) == value) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-//all cookies from game begin with "sha_deeper_" automagically
-function getCookie(name) {
-	var cookieStr = document.cookie;
-	var returnValue = "";
-	var searchFor = "sha_deeper_" + name + "=";
-	var startIndex = cookieStr.indexOf(searchFor);
-	if(startIndex != -1) {
-		startIndex += searchFor.length;
-		endIndex = cookieStr.indexOf(";", startIndex);
-		if (endIndex == -1) {
-			endIndex = cookieStr.length;
-  			returnValue = cookieStr.substring(startIndex, endIndex);
-  		}
-  	}
-  	return returnValue;
-}
-
-//all cookies from game begin with "sha_deeper_" automagically
-function setCookie(name, value, expires) {
-	var d = new Date();
-	d.setTime(d.getTime() + (expires*24*60*60*1000));
-	var e = "expires " + d.toUTCString();
-	document.cookie = "sha_deeper_" + name + "=" + value + "; " + e;
-}
-
 //------------UNIVERSAL------------
 
 function replace(name1, name2) {
